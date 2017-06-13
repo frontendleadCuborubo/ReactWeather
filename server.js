6f2813997a4365e0f715b913d0d240af73c8a89f
frontendleadCuborubo <frontendlead@cuborubo.com>
2017-06-13 22:36:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 
-//Create our app
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  if (req.headers['x-forwarded-proto'] == 'http') {
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
-    req.redirect('http://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
-  console.log('Express run on '+ PORT + ' port');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
