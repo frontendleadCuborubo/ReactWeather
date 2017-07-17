d90fca645a4510beb678451ec84b9b7d515f53e1
frontendleadCuborubo <frontendlead@cuborubo.com>
2017-07-17 23:38:09 +0300

    Add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return(
-    <h2>About us</h2>
+    <div>
+      <h1 className="text-center">About us</h1>
+      <p>Welcome to about page</p>
+    </div>
   )
 }
 
