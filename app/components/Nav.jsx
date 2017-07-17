73d22408f6eeb7acb669845bc9a71920be3d679d
frontendleadCuborubo <frontendlead@cuborubo.com>
2017-07-17 22:34:41 +0300

    Style Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -16,17 +16,67 @@ var {Link, IndexLink} = require('react-router');
 //   }
 // });
 
-var Nav = (props) => {
-  return(
-    <nav>
-      <h3>nav component</h3>
-      <ul>
-        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-        <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
-        <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
-      </ul>
-    </nav>
-  )
-}
+// var Nav = (props) => {
+//   return(
+//     <div className="top-bar">
+//       <div className="top-bar-left">
+//         <ul className="menu">
+//           <li className="menu-text">React Menu App</li>
+//           <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+//           <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+//           <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+//         </ul>
+//       </div>
+//       <div className="top-bar-right">
+//         <ul className="menu">
+//
+//         </ul>
+//       </div>
+//     </div>
+//   )
+// }
+
+var Nav = React.createClass({
+  onSearch: function(e){
+    console.log(e.type);
+  },
+  render: function(){
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Menu App</li>
+            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"/>
+              </li>
+              <li>
+                <input className="button" type="submit" value="Get Weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    )
+  }
+});
 
 module.exports = Nav;
+
+// var old_nav = (
+//   <nav>
+//     <h3>nav component</h3>
+//     <ul>
+//       <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+//       <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+//       <li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+//     </ul>
+//   </nav>
+// )
