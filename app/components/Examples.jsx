1bed2c33eea2136217236bf6fad9dab46820b90c
frontendleadCuborubo <frontendlead@cuborubo.com>
2017-07-17 23:29:41 +0300

    Foundation Grid

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div className="section">
-      <h2>Examples fck ye</h2>
+      <h1 className="text-center">Examples</h1>
+      <p>Here a few examples to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=Washington">Washington, KO</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 }
